05e602b7d3c010f0dc58453bfeec38c86b365972
Adam <shmali81@gmail.com>
2014-09-10 18:06:33 -0500

    update engine to run with continuing output from db

--- omniEngine.py
+++ omniEngine.py
@@ -1,61 +1,28 @@
-#from rpcclient import *
 from sql import *
 from csvtools import *
 
-#dbc=sql_connect()
-
-#block with first MP transaction
-firstMPtxBlock=249948
-
-#get last known block from the RPC client
-#initialBlock=0
-
-initialBlock=firstMPtxBlock
-#initialBlock=253056
-
-#MSC tx's
-#initialBlock=319100
-
-#DEx Payment block
-#initialBlock=316339
-
-endBlock=getinfo()['result']['blocks']
-#endBlock=253060
-
-#MSC TX's
-#endBlock=319105
-
-#DEx Payment block
-#endBlock=316339
-
-#get highest TxDBSerialNum (number of rows in the Transactions table)
-#TxDBSerialNum=dbSelect('select last_value from transactions_txdbserialnum_seq',None)[0][0]+1
-
-#Start at 1 since block 0 is special case
-#21479844 btc tx's before block 249948
-TxDBSerialNum=21479844
-
-appendname=str(initialBlock)+'.'+str(endBlock)
+#Need to change variables back above here if using csv
+##appendname=str(initialBlock)+'.'+str(endBlock)
 #csv output file info for tx table
-fieldnames = ['TxHash', 'Protocol', 'TxDBSerialNum', 'TxType', 'TxVersion', 'Ecosystem', 'TxSubmitTime', 
-              'TxState', 'TxErrorCode', 'TxBlockNumber', 'TxSeqInBlock']
-out_file = open('data/tx.'+appendname+'.csv', "wb") 
-tx_table = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
-tx_table.writerow(dict((fn,fn) for fn in fieldnames))
+##fieldnames = ['TxHash', 'Protocol', 'TxDBSerialNum', 'TxType', 'TxVersion', 'Ecosystem', 'TxSubmitTime', 
+##              'TxState', 'TxErrorCode', 'TxBlockNumber', 'TxSeqInBlock']
+##out_file = open('data/tx.'+appendname+'.csv', "wb") 
+##tx_table = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
+##tx_table.writerow(dict((fn,fn) for fn in fieldnames))
 
 #csv output file info for tx_addr table
-fieldnames = ['Address', 'PropertyID', 'Protocol', 'TxDBSerialNum', 'AddressTxIndex', 'AddressRole', 'BalanceAvailableCreditDebit',
-              'BalanceReservedCreditDebit', 'BalanceAcceptedCreditDebit']
-out_file = open('data/txaddr.'+appendname+'.csv', "wb")
-txaddr_table = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
-txaddr_table.writerow(dict((fn,fn) for fn in fieldnames))
+##fieldnames = ['Address', 'PropertyID', 'Protocol', 'TxDBSerialNum', 'AddressTxIndex', 'AddressRole', 'BalanceAvailableCreditDebit',
+##              'BalanceReservedCreditDebit', 'BalanceAcceptedCreditDebit']
+##out_file = open('data/txaddr.'+appendname+'.csv', "wb")
+##txaddr_table = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
+##txaddr_table.writerow(dict((fn,fn) for fn in fieldnames))
 
 #csv output file info for blocks table
-fieldnames = ['BlockNumber', 'Protocol', 'BlockTime', 'Version', 'BlockHash',
-      'PrevBlock', 'MerkleRoot', 'Bits', 'Nonce', 'Size','TxCount']
-out_file = open('data/blocks.'+appendname+'.csv', "wb")
-blocks_table = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
-blocks_table.writerow(dict((fn,fn) for fn in fieldnames))
+##fieldnames = ['BlockNumber', 'Protocol', 'BlockTime', 'Version', 'BlockHash',
+##      'PrevBlock', 'MerkleRoot', 'Bits', 'Nonce', 'Size','TxCount']
+##out_file = open('data/blocks.'+appendname+'.csv', "wb")
+##blocks_table = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
+##blocks_table.writerow(dict((fn,fn) for fn in fieldnames))
 
 #There are 2 bitcoin transactions that have duplicate hashes in different blocks. 
 #We skip them here to avoid database issues
@@ -63,6 +30,24 @@ blocks_table.writerow(dict((fn,fn) for fn in fieldnames))
 #skip={'d5d27987d2a3dfc724e359870c6644b40e497bdc0589a033220fe15429d88599': 91842,
 #      'e3bf3d07d4b0375638d5f1db5255fe07ba2c4cb067cd81b84ee974b6585fb468': 91880}
 
+#block with first MP transaction
+firstMPtxBlock=249948
+
+#get last known block processed from db
+initialBlock=dbSelect("select max(blocknumber) from blocks", None)[0][0]+1
+
+#initialBlock=firstMPtxBlock
+#initialBlock=253056
+
+endBlock=getinfo()['result']['blocks']
+#endBlock=253060
+
+#get highest TxDBSerialNum (number of rows in the Transactions table)
+TxDBSerialNum=dbSelect('select last_value from transactions_txdbserialnum_seq',None)[0][0]+1
+
+#Start at 1 since block 0 is special case
+#21479844 btc tx's before block 249948
+#TxDBSerialNum=21479844
 
 currentBlock=initialBlock
 
