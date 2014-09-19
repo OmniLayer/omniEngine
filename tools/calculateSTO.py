fd56eb1ec4bb97efd9361edd69c0353770cdfb67
Adam <shmali81@gmail.com>
2014-09-18 20:24:49 -0500

    add tool to back calculate state for STO

--- tools/calculateSTO.py
+++ tools/calculateSTO.py
@@ -13,7 +13,7 @@ for x in txs:
   TxDBSerialNum=x[1]
   Sender=x[2]
   PropertyID=x[3]
-  SentAmount=x[4]
+  SentAmount=x[4]*-1
 
   print "Processing:", TxDBSerialNum, "Property:", PropertyID, "Sender:", Sender
 
@@ -39,10 +39,15 @@ for x in txs:
   changedata=dbSelect("select address, balanceavailablecreditdebit, balancereservedcreditdebit, balanceacceptedcreditdebit "
                       "from addressesintxs atx, transactions tx where atx.txdbserialnum=tx.txdbserialnum and "
                       "atx.propertyid=%s and tx.txblocknumber > %s order by atx.txdbserialnum desc", [PropertyID, TxBlockNum])
- 
+
   for bal in changedata:
     #process each addressintxs record to reverse the balances
     Address=bal[0]
+    try:
+      test = balance[Address]['available']
+    except KeyError:
+      balance[Address]={ 'available': 0, 'reserved': 0, 'accepted': 0}
+
     if bal[1] == None:
       available = balance[Address]['available']
     else:
@@ -55,21 +60,25 @@ for x in txs:
       accepted = balance[Address]['accepted']
     else:
       accepted = balance[Address]['accepted']-bal[3]
-    
+
     balance[Address]={'available': available, 'reserved': reserved, 'accepted': accepted}
 
   #Purge the sender from our list for final conversion
   balance.pop(Address, None)
 
-  dictlist=[]
-  #Now dump the updated addressbalance to a list for sendToOwners
+  owners=[]
+  #Now dump the updated addressbalance to a list for sendToOwners call to use
   for key, value in balance.iteritems():
     if value['available'] > 0:
       temp = [key,value['available']]
-      dictlist.append(temp)
+      owners.append(temp)
 
+  #print owners
+  #print "\n\n"
+  #print "amount of tx to process", len(owners)
   #call sendToOwners and update the output
+  print "Sender:", Sender, "Amount:", SentAmount, "Prop:", PropertyID, "Prot", Protocol, "Serial:", TxDBSerialNum, "\n"
+  #raw_input('ready to process, Press <ENTER> to continue')
   sendToOwners(Sender, SentAmount, PropertyID, Protocol, TxDBSerialNum, owners)
   dbCommit()
   raw_input('Updated list, Press <ENTER> to continue')
-
