89de4bfd839f2bd7dc7885d19cc7fc83a1b5a8c8
Adam <shmali81@gmail.com>
2014-09-28 18:39:54 -0500

    Add module to get/calculate the price of sp coins

---
+++ updatePrices.py
@@ -0,0 +1,146 @@
+import requests
+import json
+import os.path
+from datetime import datetime
+from sqltools import *
+from common import *
+
+
+def updatePrices():
+  updateBTC()
+  updateMSCSP()
+  dbCommit()
+
+def fiat2propertyid(abv):
+  #create a fiat property id system and use it for storing information in database
+  try:
+    convert={"USD": 0 ,
+             "EUR": 1 ,
+             "AUD": 2 , 
+           }
+    return convert[abv.upper()]
+  except KeyError:
+    return -1
+
+def getSource(sp):
+  try:
+    convert={1:"https://masterxchange.com/api/trades.php",
+             3:"https://masterxchange.com/api/v2/trades.php?currency=maid"
+            }
+    return convert[sp]
+  except KeyError:
+    return None
+
+def upsertRate(protocol1, propertyid1, protocol2, propertyid2, rate, source):
+
+  if propertyid1 < 0 or propertyid2 < 0:
+    printdebug(("Error, can't insert invalid propertyids", propertyid1, "for", propertyid2), 4)
+    return
+
+  # if we have a record with the same exchangerate / source just update timestamp, otherwise insert new record
+  dbExecute("with upsert as "
+              "(update exchangerates set asof=DEFAULT where protocol1=%s and propertyid1=%s and "
+              " protocol2=%s and propertyid2=%s and rate1for2=%s and source=%s  returning *) "
+            "insert into exchangerates (protocol1, propertyid1, protocol2, propertyid2, rate1for2, source) select %s,%s,%s,%s,%s,%s "
+            "where not exists (select * from upsert)",
+            (protocol1, propertyid1, protocol2, propertyid2, rate, source, protocol1, propertyid1, protocol2, propertyid2, rate, source))
+
+
+def updateBTC():
+    try:
+      source='https://api.bitcoinaverage.com/all'
+      r= requests.get( source, timeout=15 )
+      value=r.json()['USD']['averages']['last']
+
+      #get our fiat property id using internal conversion schema  
+      fpid=fiat2propertyid('USD')
+
+      upsertRate('Fiat', fpid, 'Bitcoin', 0, value, source)
+
+    except requests.exceptions.RequestException, e:
+      #error or timeout, skip for now
+      printdebug(("Error updating BTC Price",e),3)
+      pass
+
+
+def updateMSCSP():
+  try:
+    #get list of smart properties we know about
+    ROWS=dbSelect("select propertyid from smartproperties where propertyid >0 order by propertyid")
+    for x in ROWS:
+
+      sp=x[0]  
+      source=getSource(sp)
+      if source != None:
+        r = requests.get( source, timeout=15 )
+        volume = 0;
+        sum = 0;
+
+        for trade in r.json():
+            volume += float( trade['amount'] )
+            sum += float( trade['amount'] ) * float(trade['price'] )
+
+        #BTC is calculated in satashis in getvalue, so adjust our value here to compensate
+  
+        #value="{:.8f}".format( sum / volume)
+        value=(sum / volume)
+      else:
+        #no Known source for a valuation, set to 0
+        value=0
+
+      upsertRate('Bitcoin', 0, 'Mastercoin', sp, value, source)
+
+  except requests.exceptions.RequestException:
+    #error or timeout, skip for now
+    printdebug(("Error updating MSCSP Prices",e),3)
+    pass
+
+
+def main():
+  lockFile='/tmp/updatePrices.lock'
+  now=datetime.now()
+
+  if os.path.isfile(lockFile):
+    #open the lock file to read pid and timestamp
+    file=open(lockFile,'r')
+    pid=file.readline().replace("\n", "")
+    timestamp=file.readline()
+    file.close()
+    #check if the pid is still running
+    if os.path.exists("/proc/"+str(pid)):
+      print "Exiting: updatePrices already running with pid:", pid, "  Last update started at ", timestamp
+    else:
+      print "Stale updatePrices found, no running pid:", pid, " Process last started at: ", timestamp
+      print "Removing lock file and waiting for restart"
+      os.remove(lockFile)
+    #exit program and wait for next run
+    exit(1)
+  else:
+    #start/create our lock file
+    file = open(lockFile, "w")
+    file.write(str(os.getpid()))
+    file.write(str(now))
+    file.close()
+
+    #set our debug level, all outputs will be controlled by this
+    setdebug(9)
+
+    try:
+      updatePrices()
+    except Exception, e:
+      #Catch any issues and stop processing. Try to undo any incomplete changes
+      print "updatePrices: Problem with ", e
+      if dbRollback():
+        print "Database rolledback"
+      else:
+        print "Problem rolling database back"
+      os.remove(lockFile)
+      exit(1)
+
+  #remove the lock file and let ourself finish
+  os.remove(lockFile)
+
+
+
+
+if __name__ == "__main__":main() ## with if
