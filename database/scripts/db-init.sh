11953f9abbf292a77047cb16eb87c62022689669
Sean Gilligan <sean@msgilligan.com>
2014-10-01 11:58:13 -0700

    Prepend ‘../‘ on the path to the .psql files

--- database/scripts/db-init.sh
+++ database/scripts/db-init.sh
@@ -3,8 +3,8 @@ set -x -e
 HOST=omniwallet-test.ccbz9vycl90c.us-west-2.rds.amazonaws.com
 OPTIONS="--echo-all --host=$HOST --port=5432 --username=xmcmaster -v ON_ERROR_STOP=1"
 echo "Init Schema..."
-psql $OPTIONS --dbname="postgres" -f omni_db_schema.psql
+psql $OPTIONS --dbname="postgres" -f ../omni_db_schema.psql
 echo "Add intial data..."
-psql $OPTIONS --dbname="omniwallet" -f omni_db_initialize_data.psql
+psql $OPTIONS --dbname="omniwallet" -f ../omni_db_initialize_data.psql
 
 
