d87ca02fcc4c8d5d2230af9d8043f294d6fc12b3
Sean Gilligan <sean@msgilligan.com>
2014-10-01 16:03:18 -0700

    Remove hard-coded settings, defer to env. vars
    PGUSER
    PGPASSWORD
    PGHOST
    PGPORT
    
    Other minor syntax simplifications

--- database/scripts/db-drop.sh
+++ database/scripts/db-drop.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-HOST=omniwallet-test.ccbz9vycl90c.us-west-2.rds.amazonaws.com
-dropdb --echo --if-exists --host=$HOST --port=5432 --username=xmcmaster omniwallet
+dropdb --echo --if-exists  omniwallet
--- database/scripts/db-init.sh
+++ database/scripts/db-init.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 set -x -e
-HOST=omniwallet-test.ccbz9vycl90c.us-west-2.rds.amazonaws.com
-OPTIONS="--echo-all --host=$HOST --port=5432 --username=xmcmaster -v ON_ERROR_STOP=1"
+OPTIONS="--echo-all -v ON_ERROR_STOP=1"
 echo "Init Schema..."
-psql $OPTIONS --dbname="postgres" -f ../omni_db_schema.psql
+psql $OPTIONS -f ../omni_db_schema.psql postgres
 echo "Add intial data..."
-psql $OPTIONS --dbname="omniwallet" -f ../omni_db_initialize_data.psql
+psql $OPTIONS -f ../omni_db_initialize_data.psql omniwallet
 
 
