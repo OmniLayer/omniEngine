3803e9f1a1d3507cb31c88e13553111589fe9168
Sean Gilligan <sean@msgilligan.com>
2014-10-29 01:45:35 -0700

    Make omni_db_createusers.psql callable from automation scripts
    * Remove hardcoded connect to database named ‘omniwallet’
    * Add ‘drop role if exists’ before creating users
    * Add PSQL variables for passwords which must be set externally
    * Update scripts/db-init.sh to call omni_db_createusers.psql

--- database/scripts/db-init.sh
+++ database/scripts/db-init.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -x -e
+if [[ -z "$OMNIDB_ENGINE_PASSWORD" ]] || [[ -z "$OMNIDB_WWW_PASSWORD" ]] ; then
+ echo "Environment variables OMNIDB_ENGINE_PASSWORD and OMNIDB_WWW_PASSWORD must be set"
+ exit 1
+fi
 OPTIONS="--echo-all -v ON_ERROR_STOP=1"
 echo "Create Database..."
 createdb "omniwallet" "Omniwallet wallet and transaction database"
@@ -7,5 +11,8 @@ echo "Init Schema..."
 psql $OPTIONS -f ../omni_db_schema.psql omniwallet
 echo "Add intial data..."
 psql $OPTIONS -f ../omni_db_initialize_data.psql omniwallet
-
+echo "Configure users and permissions..."
+psql $OPTIONS -f ../omni_db_createusers.psql \
+    --variable=omnienginePassword=\'${OMNIDB_ENGINE_PASSWORD}\' \
+    --variable=omniwwwPassword=\'${OMNIDB_WWW_PASSWORD}\' omniwallet
 
