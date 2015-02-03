87b2cee75b5931b9baa6f65bb8fdf3b7092ff9c9
Adam <shmali81@gmail.com>
2015-02-03 15:23:15 -0600

    fix crowdsale expiration check

--- sql.py
+++ sql.py
@@ -846,9 +846,10 @@ def expireCrowdsales(BlockTime, Protocol):
 
     else:
       #find the offers that are ready to expire and credit the 'accepted' amount back to the sellers sale
-      expiring=dbSelect("select propertyid from smartproperties as sp inner join transactions as tx on "
-                      "(sp.createtxdbserialnum=tx.txdbserialnum) where tx.txtype=51 and sp.protocol=%s and "
-                      "cast(propertydata::json->>'endedtime' as numeric) < %s and propertydata::json->>'active'='true'", (Protocol, BlockTime))
+      expiring=dbSelect("select propertyid from smartproperties as sp inner join transactions as tx on (sp.createtxdbserialnum=tx.txdbserialnum) " 
+                        "where tx.txtype=51 and sp.protocol=%s and propertydata::json->>'active'='true' and "
+                        "( cast(propertydata::json->>'deadline' as numeric) < %s or cast(propertydata::json->>'endedtime' as numeric) < %s)", 
+                        (Protocol, BlockTime, BlockTime))
 
       #Process all the crowdsales that should have expired by now
       for property in expiring:
