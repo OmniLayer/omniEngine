c9a39e1ee55e3f32b268b921cd6879937f5ebbaa
Adam <shmali81@gmail.com>
2016-04-28 13:29:42 -0500

    cleanup walletbackup checker

--- backupWallets.py
+++ backupWallets.py
@@ -33,9 +33,12 @@ def main():
   if os.path.isfile(lockFile):
     #open the lock file to read pid and timestamp
     file=open(lockFile,'r')
-    pid=file.readline().replace("\n", "")
-    timestamp=file.readline()
+    #pid=file.readline().replace("\n", "")
+    #timestamp=file.readline()
+    x=file.readline()
     file.close()
+    pid=x.split(",")[0]
+    timestamp=x.split(",")[1]
     #check if the pid is still running
     if os.path.exists("/proc/"+str(pid)):
       print "Exiting: backupWallet already running with pid:", pid, "  Last update started at ", timestamp
@@ -48,8 +51,9 @@ def main():
   else:
     #start/create our lock file
     file = open(lockFile, "w")
-    file.write(str(os.getpid()))
-    file.write(str(now))
+    #file.write(str(os.getpid()))
+    #file.write(str(now))
+    file.write(str(os.getpid())+","+str(now))
     file.close()
 
     #set our debug level, all outputs will be controlled by this
