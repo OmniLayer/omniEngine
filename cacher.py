a230a250b13491a270700df9023b6a34e54290a3
Adam <adam@mastercoin.org>
2017-07-10 14:02:32 -0500

    Error handling

--- cacher.py
+++ cacher.py
@@ -20,9 +20,11 @@ def rKeys(key):
   return r.keys(key)
 
 def rSetNotUpdateBTC(baldata):
-  for addr in baldata['fresh']:
-    rSet("omniwallet:balances:address:"+str(addr),json.dumps( {"bal":baldata['bal'][addr],"error":None}))
-    rExpire("omniwallet:balances:address:"+str(addr),150)
+  fresh=baldata['fresh']:
+  if fresh!=None and len(fresh)>0:
+    for addr in fresh:
+      rSet("omniwallet:balances:address:"+str(addr),json.dumps( {"bal":baldata['bal'][addr],"error":None}))
+      rExpire("omniwallet:balances:address:"+str(addr),150)
 
 def rExpireAllBalBTC():
   for addr in rKeys("omniwallet:balances:address:*"):
