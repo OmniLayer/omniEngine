97426223ef94741d3ab1327cd9db4918a3f4d1f7
Andrew Rothman <andrew@andrewrothman.com>
2017-07-13 15:42:00 -0700

    allow specifying database name in db scripts

--- database/scripts/db-console.sh
+++ database/scripts/db-console.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-psql omniwallet
+set -x -e
+if [ -z "$OMNIDB_DATABASE" ] ; then
+ echo "Environment variable OMNIDB_DATABASE must be set"
+ exit 1
+fi
+
+psql ${OMNIDB_DATABASE}
 
 
--- database/scripts/db-init.sh
+++ database/scripts/db-init.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -x -e
-if [[ -z "$OMNIDB_ENGINE_PASSWORD" ]] || [[ -z "$OMNIDB_WWW_PASSWORD" ]] ; then
- echo "Environment variables OMNIDB_ENGINE_PASSWORD and OMNIDB_WWW_PASSWORD must be set"
+if [[ -z "$OMNIDB_ENGINE_PASSWORD" ]] || [[ -z "$OMNIDB_WWW_PASSWORD" ]] || [ -z "$OMNIDB_DATABASE" ] ; then
+ echo "Environment variables OMNIDB_ENGINE_PASSWORD, OMNIDB_WWW_PASSWORD, and OMNIDB_DATABASE must be set"
  exit 1
 fi
 OPTIONS="--echo-all -v ON_ERROR_STOP=1"
 echo "Create Database..."
-createdb "omniwallet" "Omniwallet wallet and transaction database"
+createdb "${OMNIDB_DATABASE}" "Omniwallet wallet and transaction database"
 echo "Init Schema..."
-psql $OPTIONS -f ../omni_db_schema.psql omniwallet
+psql $OPTIONS -f ../omni_db_schema.psql ${OMNIDB_DATABASE}
 echo "Add intial data..."
-psql $OPTIONS -f ../omni_db_initialize_data.psql omniwallet
+psql $OPTIONS -f ../omni_db_initialize_data.psql ${OMNIDB_DATABASE}
 echo "Configure users and permissions..."
 psql $OPTIONS -f ../omni_db_createusers.psql \
     --variable=omniengine=${OMNIDB_ENGINE_USER} \
     --variable=omnienginePassword=\'${OMNIDB_ENGINE_PASSWORD}\' \
     --variable=omniwww=${OMNIDB_WWW_USER} \
-    --variable=omniwwwPassword=\'${OMNIDB_WWW_PASSWORD}\' omniwallet
+    --variable=omniwwwPassword=\'${OMNIDB_WWW_PASSWORD}\' ${OMNIDB_DATABASE}
 
