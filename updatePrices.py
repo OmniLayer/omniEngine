e3f00d21535d3966e8339fd9c288aa42a42bef0c
Adam <achamely@users.noreply.github.com>
2017-11-27 09:13:24 -0500

    update safex source

--- updatePrices.py
+++ updatePrices.py
@@ -97,12 +97,13 @@ def getSource(sp):
   try:
     #convert={1:"https://masterxchange.com/api/trades.php",
     #         3:"https://masterxchange.com/api/v2/trades.php?currency=maid"
-    #         56:"https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-SEC&count=100",
+    #         
     #         89:"https://api.livecoin.net/exchange/last_trades?currencyPair=DIBC/BTC",
     #        }
     convert={1:"https://poloniex.com/public?command=returnTradeHistory&currencyPair=BTC_OMNI",
              3:"https://poloniex.com/public?command=returnTradeHistory&currencyPair=BTC_MAID",
              39:"https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-AMP&count=100",
+             56:"https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-SAFEX&count=100",
              58:"https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-AGRS&count=100",
              59:"https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-PDC&count=100",             
              90:"https://market.bitsquare.io/api/trades?market=sfsc_btc"
